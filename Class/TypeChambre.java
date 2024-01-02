5af6fafc720c9dd94abc2add0df7e90cf06d0f51
Ouatson <ouatsonbg@gmail.com>
2024-01-02 16:03:14 +0100

    Modifications Chambres... ajout Enumération TypeChambre

---
+++ Class/TypeChambre.java
@@ -0,0 +1,13 @@
+package Class;
+
+public enum TypeChambre {
+    LUXE("luxe"),
+    NORMAL("normal");
+
+    private String type;
+
+    private TypeChambre(String type) {
+        this.type = type;
+    }
+
+}
