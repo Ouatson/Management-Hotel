de10315c708b13ebfdd2225c687a6f8fcd776d9a
IdrissNAMEN <106448425+IdrissNAMEN@users.noreply.github.com>
2024-01-03 15:22:38 +0100

    Création des constructeurs pourc les classses Commandes et Clients

--- Class/Clients.java
+++ Class/Clients.java
@@ -11,4 +11,64 @@ public class Clients {
     private Date date_arrivee;
     private Date date_sortie;
     private String mode_paiement; // Enumeration
+
+
+    public Clients(int id, String nom, String prenom, String email, String telephone, Date date_arrivee, Date date_sortie, String mode_paiement){
+        this.id = id;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+        this.telephone = telephone;
+        this.date_arrivee = date_arrivee;
+        this.date_sortie = date_sortie;
+        this.mode_paiement = mode_paiement;
+    }
+
+    public int getid(){
+        return id;
+    }
+
+    public int setid(){
+        return id;
+    }
+
+    public String getnom(){
+        return nom;
+    }
+
+    public String setnom(){
+        return nom;
+    }
+
+    public String getemail(){
+        return email;
+    }
+
+    public String setemail(){
+        return email;
+    }
+
+    public String gettelephone(){
+        return telephone;
+    }
+
+    public String settelephone(){
+        return telephone;
+    }
+
+    public Date getdate_arrivee(){
+        return date_arrivee;
+    }
+
+    public Date setdate_sortie(){
+        return date_sortie;
+    }
+
+    public String getmode_paiement(){
+        return mode_paiement;
+    }
+
+    public String setmode_paiement(){
+        return mode_paiement;
+    }
 }
--- Class/Commandes.java
+++ Class/Commandes.java
@@ -8,4 +8,82 @@ public class Commandes {
     private int id_client;
     private int id_chambre;
     private int id_hotel;
+
+
+
+
+    public Commandes(int id, String type, float montant, String articles, int id_client, int id_chambre, int id_hotel){
+        this.id = id;
+        this.type = type;
+        this.montant = montant;
+        this.articles = articles;
+        this.id_client = id_client;
+        this.id_chambre = id_chambre;
+        this.id_hotel = id_hotel;
+        
+    }
+
+    public int id(){
+    return id;
 }
+
+    public int getid(){
+        return id;
+    }
+
+    public int setid(){
+        return id;
+    }
+
+    public String gettype(){
+        return type;
+
+    }
+
+    public String settype(){
+        return type;
+    }
+
+    public float getmontant(){
+        return montant;
+    }
+
+    public float setmontant(){
+        return montant;
+    }
+
+    public String getarticles(){
+        return articles;
+    }
+
+    public String setarticles(){
+        return articles;
+    }
+
+    public int getid_client(){
+        return id_client;
+    }
+
+    public int setid_client(){
+        return id_client;
+    }
+
+    public int getid_chambre(){
+        return id_chambre;
+    }
+
+    public int setid_chambre(){
+        return id_chambre;
+    }
+
+    public int getid_hotel(){
+        return id_hotel;
+    }
+
+    public int setid_hotel(){
+        return id_hotel;
+    }
+}
+
+
+
