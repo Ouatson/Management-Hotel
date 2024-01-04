db1c8003af95a4d747368a38102592ec7154fc79
Ouatson <ouatsonbg@gmail.com>
2024-01-05 00:59:17 +0100

    AJout de la classe ServiceUtilisateurs.java avec le traitement de fichier et la méthode add... en premier lieu

---
+++ Class/ServiceUtilisateur.java
@@ -0,0 +1,47 @@
+package Class;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class ServiceUtilisateur {
+
+    // Chemin du fichier Excel existant
+    private static String excelFilePath = "mon_classeur.xlsx";
+
+    // Nom de la feuille de calcul à modifier
+    private static String sheetName = "Utilisateurs";
+    
+    public static void addDataToExistingSheet(Utilisateurs user) {
+
+        try (FileInputStream fileInputStream = new FileInputStream(excelFilePath);
+                Workbook workbook = new XSSFWorkbook(fileInputStream)) {
+
+            // Récupération de la feuille de calcul
+            Sheet sheet = workbook.getSheet(sheetName);
+
+            if (sheet == null) {
+                System.out.println("La feuille de calcul " + sheetName + " n'a pas été trouvée.");
+                return;
+            }
+
+            // Ajout de nouvelles données à la feuille de calcul
+            Row newRow = sheet.createRow(sheet.getLastRowNum() + 1);
+            newRow.createCell(0).setCellValue(1); // ID de l'utilisateur
+            newRow.createCell(1).setCellValue("OUATTARA"); // Nom de l'utilisateur
+            newRow.createCell(2).setCellValue("Lancina"); // Prenom  de l'utilisateur
+
+            // ...
+
+            // Sauvegarde des modifications dans le même fichier Excel
+            try (FileOutputStream fileOutputStream = new FileOutputStream(excelFilePath)) {
+                workbook.write(fileOutputStream);
+            }
+
+            System.out.println("Données ajoutées avec succès à la feuille de calcul " + sheetName);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
